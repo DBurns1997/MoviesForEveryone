cadff6f7a605b26870512806145ee90508100a3a
UnhappyCacodemon <shiftyhyena@gmail.com>
2021-02-27 13:43:51 -0800

    Added, Asp.Net Identity NuGet package, and register/login pages

---
+++ MoviesForEveryone/Pages/Login.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MoviesForEveryone.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+    }
+}
